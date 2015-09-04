ee9e66d202fb8b21c68134fc4339f5ef491ddbb0
Daniele Pizzolli <daniele.pizzolli@create-net.org>
2015-09-04 09:41:41 +0200

    Update example config

--- custom-itbox-example.mk
+++ custom-itbox-example.mk
@@ -1,12 +1,16 @@
-BUILD_DIR?=/opt/build/bd
-LOCAL_MIRROR?=/opt/build/lm
+BUILD_DIR=/opt/build/bd
+LOCAL_MIRROR=/opt/build/lm
 
-FUELLIB_COMMIT?=si/2.0
-NAILGUN_COMMIT?=si/2.0
+FUELLIB_COMMIT=si/2.1
+NAILGUN_COMMIT=si/2.1
 
-FUELLIB_REPO?=https://github.com/SmartInfrastructures/fuel-libray-dev.git
-NAILGUN_REPO?=https://github.com/SmartInfrastructures/fuel-web-dev.git
+FUELLIB_REPO=https://github.com/SmartInfrastructures/fuel-library-dev.git
+NAILGUN_REPO=https://github.com/SmartInfrastructures/fuel-web-dev.git
 
-EXTRA_DEB_REPOS?=http://repo.create-net.org/si/2.0 precise custom
+FUELLIB_REPO=https://github.com/dancn/fuel-library-dev.git
+NAILGUN_REPO=https://github.com/dancn/fuel-web-dev.git
 
-FEATURE_GROUPS?=experimental,fiware
+
+EXTRA_DEB_REPOS=http://repo.create-net.org/si/2.0 precise custom|http://us.archive.ubuntu.com/ubuntu/ precise-updates main|http://security.ubuntu.com/ubuntu/ precise-security main|http://us.archive.ubuntu.com/ubuntu/ precise main
+
+FEATURE_GROUPS=experimental,fiware
